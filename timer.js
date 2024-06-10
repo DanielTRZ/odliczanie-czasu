cf20c6db0e26e68b896a92cf09611e4f7cbe5ae3
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-06-10 19:45:16 +0200

    Update timer.js

--- timer.js
+++ timer.js
@@ -9,7 +9,7 @@ var godzina = dzisiaj.getHours();
 if (godzina<10) godzina = "0"+godzina;
 var minuta = dzisiaj.getMinutes();
 if (minuta<10) minuta = "0"+minuta;
-		var sekunda = dzisiaj.getSeconds();
+var sekunda = dzisiaj.getSeconds();
 		if (sekunda<10) sekunda = "0"+sekunda;
 		
 		document.getElementById("zegar").innerHTML = 
