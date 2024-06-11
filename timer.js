bebee043beb63486e861784e8e5113c76ad09a24
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-06-11 19:16:41 +0200

    Update timer.js

--- timer.js
+++ timer.js
@@ -15,5 +15,5 @@ if (sekunda<10) sekunda = "0"+sekunda;
 document.getElementById("zegar").innerHTML = 
 dzien+"/"+miesiac+"/"+rok+" | "+godzina+":"+minuta+":"+sekunda;
 		 
-		 setTimeout("odliczanie()",1000);
+setTimeout("odliczanie()",1000);
 	}
